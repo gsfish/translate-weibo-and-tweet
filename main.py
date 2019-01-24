9dde71a6b71bfe5c2c439943fa9b8dc059155383
gsfish <caoyu97@hotmail.com>
2019-01-25 00:45:53 +0800

    update code

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ import requests
 from flask import *
 
 
+# in post body when webhook send requests, and ifttt filter after webhook triggered
 AUTH_KEY = ''
-
+# for youdao cloud api
 YOUDAO_APP_KEY = ''
 YOUDAO_SECRET_KEY = ''
 YOUDAO_API = 'https://openapi.youdao.com/api'
-
+# for ifttt webhooks service
 WEBHOOK_KEY = ''
 WEBHOOK_EVENT_TEXT = 'text_weibo_posted'
 WEBHOOK_EVENT_IMAGE = 'image_weibo_posted'
@@ -25,17 +26,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def translate_and_tweet():
+    # validate auth_key
     data = request.get_json()
     if data['auth_key'] != AUTH_KEY:
-        return jsonify({
-            'detail': 'Invalid auth_key {0!r}'.format(data['auth_key'])
-        }), 401
+        msg = 'Invalid auth_key {0!r}'.format(data['auth_key'])
+        app.logger.warning(msg)
+        return jsonify({'detail': msg}), 401
 
+    # translate weibo text into english
     text_raw = data['text']
     tran_salt = str(random.randint(1, 65536))
     tran_hash_str = (YOUDAO_APP_KEY+text_raw+tran_salt+YOUDAO_SECRET_KEY).encode('utf-8')
     tran_sign = hashlib.md5(tran_hash_str).hexdigest()
-    tran_data = {
+    tran_payload = {
         'q': text_raw,
         'from': 'auto',
         'to': 'EN',
@@ -44,40 +47,40 @@ def translate_and_tweet():
         'sign': tran_sign,
     }
     try:
-        tran_resp = requests.post(YOUDAO_API, data=tran_data)
+        tran_resp = requests.post(YOUDAO_API, data=tran_payload)
     except Exception as e:
-        return jsonify({
-            'detail': str(e)
-        }), 500
-
-    tran_res = tran_resp.json()
-    if tran_res['errorCode'] != '0':
-        return jsonify({
-            'detail': 'Translation Error: Failed to translate {0!r}'.format(text_raw)
-        }), 500
-
-    text_tran = tran_res['translation'][0]
-    image_url = data.get('image_url', None)
-    if image_url:
-        url = WEBHOOK_URL_IMAGE
+        msg = str(e)
+        app.logger.error(msg)
+        return jsonify({'detail': msg}), 500
     else:
-        url = WEBHOOK_URL_TEXT
-    payload = {
+        tran_res = tran_resp.json()
+
+    if tran_res['errorCode'] != '0':
+        msg = 'Translation Error: Failed to translate {0!r}'.format(text_raw)
+        app.logger.error(msg)
+        return jsonify({'detail': msg}), 500
+
+    # post a tweet
+    post_text = tran_res['translation'][0]
+    post_image_url = data.get('image_url', None)
+    if post_image_url:
+        post_url = WEBHOOK_URL_IMAGE
+    else:
+        post_url = WEBHOOK_URL_TEXT
+    post_payload = {
         'value1': AUTH_KEY,
-        'value2': text_tran,
-        'value3': image_url
+        'value2': post_text,
+        'value3': post_image_url
     }
 
     try:
-        requests.post(url, json=payload)
+        requests.post(post_url, json=post_payload)
     except Exception:
-        return jsonify({
-            'detail': 'POST Error: Failed to trigger webhook service'
-        }), 500
-
-    return jsonify({
-        'detail': 'Success.'
-    }), 200
+        msg = 'POST Error: Failed to trigger webhook service'
+        app.logger.error(msg)
+        return jsonify({'detail': msg}), 500
+    else:
+        return jsonify({'detail': 'Success.'}), 200
 
 
 if __name__ == '__main__':
